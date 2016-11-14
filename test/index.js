df53a39bcbe0763b0b85f69563590d919f4ef9ae
Kosntantin V. Borisov <konstantin.borisov@hitsoft-it.com>
2016-11-14 13:54:55 +0700

    + npm package

---
+++ test/index.js
@@ -0,0 +1,9 @@
+var should = require('chai').should(),
+    ogxt = require('../src/index');
+
+describe('test', () => {
+    it('works', () => {
+        const five = 5;
+        five.should.equal(5);
+    });
+});
