86650a9306f0c2eac3c035284b099d1fe8da1871
Maxim <rimaxaurit@gmail.com>
2025-02-12 15:06:42 +0700

    update lib from src

--- lib/annotate.d.ts
+++ lib/annotate.d.ts
@@ -1,7 +1,60 @@
 import { Annotations } from './model';
+export interface EditorAnnotation {
+    id: number;
+    alternateId?: string;
+    tuid?: string;
+    idx: number;
+    kind: string;
+    group: string;
+    intensity?: number;
+    classes: string;
+    attr: {
+        start: boolean;
+        end: boolean;
+        large: boolean;
+    };
+}
+export interface EditorSpan {
+    start: number;
+    end: number;
+    annotations: EditorAnnotation[];
+}
+export interface SpanSuggestion {
+    start: number;
+    end: number;
+    suggestions: {
+        annotationId: number;
+        suggestions: {
+            idx: number;
+            value: string;
+        }[];
+    }[];
+}
+export interface HtmlWithEditorSpan {
+    html: string;
+    spans: EditorSpan[];
+}
 /**
  * Добавляет в `html` аннотации `annotations` в виде span-ов со специфическими стилями для последующей визуализации отчёта
+ * При этом возвращает группировки span-ов, чтобы вызывающему коду можно было понять структуру без парсинга html
+ * @param html
+ * @param annotations
+ * @return HtmlWithEditorSpan
+ */
+export declare function annotate_and_get_html_with_spans(html: string, annotations: Annotations): HtmlWithEditorSpan;
+/**
+ * Добавляет в `html` аннотации `annotations` в виде span-ов со специфическими стилями для последующей визуализации отчёта.
+ * Возвращает только html строку
  * @param html
  * @param annotations
  */
 export declare function annotate(html: string, annotations: Annotations): string;
+/**
+ * Добавляет в `html` аннотации `annotations` в виде span-ов со специфическими стилями для последующей визуализации отчёта
+ * При этом перед формированием html вызывает переданную функцию разбиения совета по span-ам, на которые разбивается аннотация.
+ * Возвращает html строку, там, где совет разбивается и его можно применить по частям, добавляются атрибуты с советами
+ * @param html
+ * @param annotations
+ * @param splitSuggestion
+ */
+export declare function annotate_with_suggestion(html: string, annotations: Annotations, splitSuggestion: (spans: EditorSpan[], annotations: Annotations) => SpanSuggestion[]): string;
--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,5 +1,5 @@
 export { parseTitle } from './parseTitle';
 export { cleanupHtml, cleanupAnnotations } from './cleanup';
 export { html2ogxt } from './html2ogxt';
-export { annotate } from './annotate';
+export { annotate, annotate_and_get_html_with_spans, annotate_with_suggestion, SpanSuggestion, EditorSpan } from './annotate';
 export type { Annotation, Annotations, Position } from './model';
